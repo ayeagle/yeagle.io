05d72b36054b4cab73308466e6008f5471dd3d04
Alex Yeagle <yeagle@fb.com>
2022-10-30 12:46:50 -0700

    committing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@styles/globals.css'
 
+// const button = document.getElementById('button1')
+
+// // button.addEventListener('click', function() {
+// //     alert("NICE")
+
+// //     document.body.style.backgroundColor = 'olive'
+// // })
+
+
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
