b32a608bbeb232faa6e53e7105334048367fc675
Alex Yeagle <yeagle@fb.com>
2022-10-30 12:47:47 -0700

    made changes

---
+++ components/Button.js
@@ -0,0 +1,10 @@
+
+const Button = ( {handleClick} ) => { //args are called props
+  return (
+    <>
+      <button id='button1' onClick= {handleClick}>Click Me!</button>
+    </>
+  )
+}
+
+export default Button
