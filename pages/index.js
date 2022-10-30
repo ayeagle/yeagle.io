f13b9dd676332a60c71ab2bdd798202ccd3f6447
Alex Yeagle <yeagle@fb.com>
2022-10-30 12:43:10 -0700

    commit
    commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,34 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Button from '@components/Button'
+import Input from '@components/Input'
+
+import { useState, useEffect } from 'react'
+
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const [workTime, setWorkTime] = useState(0);
+  const [breakTime, setBreakTime] = useState(0);
+  const [iterations, setIterations] = useState(0);
+
+  const handleClick = () => setCount(count+1);
+  // const handleInputClick = (e) => {
+  //   console.log("val1 is " + e.target.value)
+  //   setGoal(e.target.value)
+  //   console.log("val1 is " + e.target.value)
+  // };
+
+  const handleInputClick = (val1) => {
+    console.log("val1 is " + val1)
+    setWorkTime(val1)
+    console.log("val1 is " + val1)
+    console.log("this is the goal "+workTime)
+  };
+
+  // const updateTime = setTimeLeft(timeLeft-1);
+
   return (
     <div className="container">
       <Head>
@@ -11,13 +37,29 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
+        <Header title="Welcome to PomoDomo" />
+        {/* <p className="description">
           Get started by editing <code>pages/index.js</code>
-        </p>
+        </p> */}
+        <p>Count is {count}</p>
+
+        <Button handleClick={handleClick}/>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <Input handleInputClick={handleInputClick} inputPrompt={"How long do you want to work?"}/>
+        {/* <div>{val1}</div> */}
+        {/* how to get component value returned so it can be dynamically displayed on the page after */}
+        <Input handleInputClick={handleInputClick} inputPrompt={"How long do you want to break?"} />
+        <Input handleInputClick={handleInputClick} inputPrompt={"How many cycles would you like to do?"} />
+        {/* <p> Time remaining is :{timeLeft}</p> */}
+
+
       </main>
 
       <Footer />
+      {/* <script src="jsapp.js"></script> */}
     </div>
   )
 }
