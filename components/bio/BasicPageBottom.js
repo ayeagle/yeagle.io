82dfc90879ef4f2e764db29b84c28d483f2b13ef
Alex Yeagle <yeagle@fb.com>
2022-12-19 11:24:52 -0800

    just updating master branch with socials stuff

--- components/bio/BasicPageBottom.js
+++ components/bio/BasicPageBottom.js
@@ -1,21 +1,23 @@
 import { MetaHTMLAttributes } from "react"
 import Footer from "@components/pomodomo/Footer"
 import styles from './BasicPageBottom.module.css'
+import Socials from "./Socials"
 
 
 export default function BasicPageBottom() {
   return (
     <>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
 
 
-        <p > Home </p>
-        <p  > Portfolio </p>
-        <p  > Resume </p>
-        <p  > Creative Work </p>
-        <p  > Contact </p>
+      <p > Home </p>
+      <p  > Portfolio </p>
+      <p  > Resume </p>
+      <p  > Creative Work </p>
+      <p  > Contact </p>
+      <Socials size={"3vw"}/>
       <footer className={styles.footer}>
         Made by <img src="/bagel_logo.png" alt="Netlify Logo" className={styles.logo} /> for you
       </footer>    </>
--- components/bio/Socials.js
+++ components/bio/Socials.js
@@ -1,38 +1,21 @@
 import React from "react"
+import * as icons from 'react-icons/fa';
+import styles from './Socials.module.css'
 
-export default function Socials() {
+export default function Socials({size,loc}) {
+    console.log(size + "?this is the size prop")
+    console.log(loc + "?this is the loc prop")
+
     return (
         <>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
-            <div>
-
-
-
-                <a class="btn btn-primary" style={{backgroundColor: "#3b5998"}} href="#!" role="button"
-                ><i className ="fa-facebook-f"></i></a>
-                {React.createElement('i', { className: 'fab fa-facebook-f' })}
-                {React.createElement('i', { className: 'fab fa-facebook-f' })}
-                {React.createElement('i', { className: 'fab fa-twitter' })}
-                {React.createElement('i', { className: 'fab fa-google' })}
-                {React.createElement('i', { className: 'fab fa-instagram' })}
-                {React.createElement('i', { className: 'fab fa-linkedin-in' })}
-                {React.createElement('i', { className: 'fab fa-pinterest' })}
-                {React.createElement('i', { className: 'fab fa-vk' })}
-
-                <i class="fab fa-facebook-f"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-google"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-linkedin-in"></i>
-                <i class="fab fa-pinterest"></i>
-                <i class="fab fa-vk"></i>
-                <i class="fab fa-stack-overflow"></i>
-                <i class="fab fa-youtube"></i>
-                <i class="fab fa-slack-hash"></i>
-                <i class="fab fa-github"></i>
-                <i class="fab fa-dribbble"></i>
-                <i class="fab fa-reddit-alien"></i>
-                <i class="fab fa-whatsapp"></i>
+            {/* <icons.FaFacebook /> */}
+            {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" /> */}
+            <div className={styles.social_container} style={{textAlign:loc}}>
+                <div className={styles.social} style={{fontSize:size, justifyContent:loc}}> <a href="https://www.yeaglesbagels.com/about"><icons.FaCodeBranch style={{fill:"white"}} /></a></div>
+                <div className={styles.social} style={{fontSize:size, justifyContent:loc}}> <a href="https://twitter.com/AlexYeagle"><icons.FaTwitter style={{fill:"white"}}/></a></div>
+                <div className={styles.social} style={{fontSize:size, justifyContent:loc}}> <a href="https://www.linkedin.com/in/ayeagle/"><icons.FaLinkedin style={{fill:"white"}}/></a></div>
+                <div className={styles.social} style={{fontSize:size, justifyContent:loc}}> <a href="https://github.com/ayeagle"><icons.FaGithub style={{fill:"white"}}/></a></div>
+                <div className={styles.social} style={{fontSize:size, justifyContent:loc}}> <a href="https://www.instagram.com/alexyeagle/"><icons.FaInstagram style={{fill:"white"}}/></a></div>
             </div>
         </>
     )
