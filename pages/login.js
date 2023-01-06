58c299d88bba2c139ed8631ae8ed6d6220125bd3
ayeagle <alexyeagle@gmail.com>
2023-01-06 08:23:20 -0800

    pushing updates for machine

--- pages/login.js
+++ pages/login.js
@@ -16,26 +16,17 @@ import Typing from "@components/bio/Typing";
 import Flood from "@components/bio/Flood";
 import Flood2 from "@components/bio/Flood2";
 import { useSelector, useDispatch } from "react-redux";
-// import {StartDatabaseConnection} from "../components/DBcomponents/Connect"
-import Connect from "@components/DBcomponents/Connect";
+import Spacer from "@components/bio/Spacer";
+import axios from "node_modules/axios/index";
 import LogActivity from "@components/DBcomponents/LogActivity";
-import LoginButton from "@components/bio/LoginButton";
 
-// StartDatabaseConnection()
-
-let userId = 20
-
-export default function Debug() {
-
+export default function Login() {
     const [height, updateHeight] = useState(0)
     const [width, updateWidth] = useState(0)
     const dispatch = useDispatch();
     const state = useSelector((state) => state);
     const [limiter, setLimiter] = useState(0)
 
-    // console.log("this is the height (start) ==> " + height
-    //     console.log("this is the width (start) ==> " + width)
-    const [CID, setCID] = useState(0);
 
     useEffect(() => {
         // Update the height and width state when the component is mounted
@@ -50,14 +41,29 @@ export default function Debug() {
             updateWidth(window.innerWidth)
         }
 
-        if (limiter <=3 ) {
-            setLimiter(limiter + 1)
-            LogActivity(userId, "loaded debug page")
-        }
-
-        setCID(document.cookie)
         // Add the event listener
         window.addEventListener('resize', handleWindowResize)
+
+        let userId = 20 //document.cookie.substr( document.cookie.indexOf("=")+1, document.cookie.indexOf("=") + 36)
+        // let activity = "loading the about page"
+        let timestamp = new Date().toISOString()
+
+        // console.log("CLIENT SIDE: session id value is " +userId + " and the type of this is " + typeof(userId))
+        // console.log("CLIENT SIDE: activity value is " +activity + " and the type of this is " + typeof(activity))
+        // console.log("CLIENT SIDE: timestamp value is " +timestamp + " and the type of this is " + typeof(timestamp))
+
+
+
+        if (limiter <=3 ) {
+            setLimiter(limiter + 1)
+            LogActivity(userId, "loaded login page")
+        }
+
+
+
+
+
+
 
         // Clean up the event listener when the component unmounts
         return () => {
@@ -73,23 +79,16 @@ export default function Debug() {
             <BasicPageTop />
             <NavBar />
             <SVGSpacers type="bot" num="2" width={width} />
+           
+            <Spacer />
+            <Spacer />
 
-            <Connect />
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <LoginButton />
-            <br></br>
-            <br></br>
-            <br></br>
+            <Spacer />
 
-            <div style={{ height: "80vh", fill: "black", zIndex: 500, }} >
+            <Spacer />
 
-                <div>This is your uuid lol <br></br>{state.uuid}</div>
-                <div>This is your cookie lol (static) <br></br>{CID}</div>
+            <Spacer />
 
-            </div>
 
             <SVGSpacers type="top" num="2" width={width} />
             <div className={styles.box}>
