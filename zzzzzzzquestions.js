b0f97a7dfb61b58f504d10c3e0d50a9ed507fbfa
ayeagle <alexyeagle@gmail.com>
2023-01-06 19:17:30 -0800

    more login changes

---
+++ zzzzzzzquestions.js
@@ -0,0 +1,21 @@
+/*
+react router
+browser router
+usehistory?
+usenavigate?
+
+
+SSL issues
+
+promises explained
+
+
+
+manager user details after being logged in
+global object or state stuff? probably don't want to query each time
+
+
+infosec with connection strings/passwords/keys etc
+currently have keys and AWS passwords on github
+
+*/
