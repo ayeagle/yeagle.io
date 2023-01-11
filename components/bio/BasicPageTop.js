ec2a4dc34d2041222e93bd13ab8c51f0d4753012
ayeagle <alexyeagle@gmail.com>
2023-01-11 11:20:04 -0800

    portfolio fixes

--- components/bio/BasicPageTop.js
+++ components/bio/BasicPageTop.js
@@ -9,10 +9,13 @@ export default function BasicPageTop() {
     <>
       <GenSessionUuid />
       <Head>
+        <meta property="og:image" content="/bagel_logo.png" />
+        <meta property="og:title" content="Yeagle's Bagels" />
+
+        <link rel="icon" href="/bagel_logo.png" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>Alex Yeagle</title>
-        <link rel="icon" href="/bagel_logo.png" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
       </Head>
     </>
